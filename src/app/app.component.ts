3b6268b7845e71529ab0f820ddc91e6b4b41eebf
paul <taiwantom20536@gmail.com>
2018-06-16 12:30:34 +0800

    當使用者在網頁右側的關鍵字搜尋輸入文字時，會自動在輸入框的正下方顯示正在輸入的字元數。 當使用者在輸入框按下 Escape 按鍵時，會自動清空輸入框的內容。

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,20 @@ export class AppComponent {
   title = 'app';
   url = 'http://blog.miniasp.com/';
   imgurl = '/assets/images/logo.png';
+  keyword = '';
   constructor() {
   }
   changeTitle(altKey: boolean) {
     if (altKey) {
       this.title  = 'The Will Will Web';
+    }
+  }
+  changeKeyword($event: KeyboardEvent) {
+    const escapeKeyCode = 27;
+    if ($event.keyCode === escapeKeyCode) {
+      this.keyword = '';
+    } else {
+      this.keyword = ($event.target as HTMLInputElement).value;
     }
   }
 }
